50f7367dfafdb2392bd56d40a3ad0d89f4957e87
Kieran Wood <kieranw098@gmail.com>
2019-11-13 23:40:13 -0700

    Started module 2 challenge and exercises. Updated main readme

--- 2. Operators and conditionals/exercises.py
+++ 2. Operators and conditionals/exercises.py
@@ -1,1 +1,20 @@
-# Exercises have not been developed yet
+"""
+    =========== Exercise 1 =============
+
+    ...
+"""
+
+
+
+"""
+    =========== Exercise 2 =============
+
+    Take input from the command line, and convert it to
+    an int. Now pick a range (i.e. 0-10), and create a set
+    of conditional statements that prints the string
+    representation of the number input by the user.
+
+    For example if someone put in 8, then it would print 'eight'.
+    
+    Hint: Use if, elif and else statements.
+"""
