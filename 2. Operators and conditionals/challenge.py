60b94ce827ca5b4265d832b5765929aba7bbeccf
Kieran Wood <kieranw098@gmail.com>
2019-11-15 23:09:07 -0700

    Added challenges and exercises to module 2

--- 2. Operators and conditionals/challenge.py
+++ 2. Operators and conditionals/challenge.py
@@ -10,5 +10,16 @@
 """
     =========== Challenge 2 =============
 
-    ...
+    Take an input from the command line, and 
+    convert it to an int. Validate the number
+    is within the range 1-5, and then for each
+    possible value (1-5), write the code to make
+    the input 11.
+
+    I.e. Someone inputs 1 the result is 11, if someone
+    inputs 2 the result is 11 etc. If someone puts in a
+    number not in range (1-5) print:
+        'value not between 1-5 please try again'
+    
+    Hint: You should have between 6-7 if/elif/else statements
 """
