6f7836e06509547b2bc5cb770564b876f3665865
Kieran Wood <kieranw098@gmail.com>
2019-12-27 22:57:16 -0700

    Added Module 7 challenges and exercises

--- 7. Extras/challenge.py
+++ 7. Extras/challenge.py
@@ -1,1 +1,20 @@
-# Challenges have not been developed yet
+"""
+    =========== Challenge 1 =============
+
+    Below is a game in which you specify
+    how many cookies there are at the beginning.
+    on each 'turn' a random amount between 1-10
+    of cookies are removed. Once there are no cookies
+    use an f-string to print if player 1 (an even number
+    of turns) or player 2 (an odd number of turns) won. 
+"""
+import random
+
+cookies = int(input("How many cookies?: "))
+
+turns = 0
+
+while cookies < 0:
+
+    cookies -= random.randint(0,10)
+    turns += 1
--- 7. Extras/exercises.py
+++ 7. Extras/exercises.py
@@ -1,1 +1,15 @@
-# Exercises have not been developed yet
+"""
+    =========== Exercise 1 =============
+
+    Take the two variables (temperature and name),
+    and create a greeting using f-strings.
+
+    For example it could print:
+    'Hello Kieran, the temperature is 21 degrees'
+"""
+
+name = input("What is your name?: ")
+
+temperature = input("What is the temperature?: ")
+
+print("") # Enter your f-string here
